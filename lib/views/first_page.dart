f48ec3a2be3a972c725b0e8b35db2d7b54fc69da
mennasaad3694-bit <mennasaad3694@gmail.com>
2026-08-20 01:17:13 +0300

    Done Done

--- lib/views/first_page.dart
+++ lib/views/first_page.dart
@@ -7,64 +7,78 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFE8E2E8),
+      backgroundColor: const Color(0xFFE8E2E8),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 25),
-          child: Column(
-            children: [
-              Spacer(),
-              Image.asset('assets/images/task_image.png', height: 300),
-              SizedBox(height: 40),
-
-              Text(
-                'Create free notes & collaborate\nwith your team',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 30),
-
-              Text(
-                'Capture your thoughts, organize your daily tasks, and keep everything in one place',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: const Color.fromARGB(255, 18, 1, 1),
-                ),
-              ),
-              Spacer(),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
-                  );
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 55,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 159, 147, 181),
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Get Started",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: IntrinsicHeight(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 25,
+                    ),
+                    child: Column(
+                      children: [
+                        const Spacer(),
+                        Image.asset(
+                          'assets/images/task_image.png',
+                          height: 350,
+                        ),
+                        const SizedBox(height: 30),
+                        const Text(
+                          'Create free notes & collaborate\nwith your team',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        const Text(
+                          'Capture your thoughts, organize your daily tasks, and keep everything in one place',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 18, color: Colors.black54),
+                        ),
+                        Spacer(),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const HomePage(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            width: double.infinity,
+                            height: 55,
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 159, 147, 181),
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                "Get Started",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
