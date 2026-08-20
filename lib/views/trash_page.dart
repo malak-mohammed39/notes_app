937cf7dbb8517033f1eb2905f7adb9b1920fefcf
Malak Mohammed <momen692014@gmail.com>
2026-08-20 03:37:56 +0300

    Complete trash page

--- lib/views/trash_page.dart
+++ lib/views/trash_page.dart
@@ -1,13 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-//import 'package:notes_app/views/trash_page.dart';
 import '../cubits/notes_cubit/notes_cubit.dart';
 import '../cubits/notes_cubit/notes_state.dart';
-//import '../models/note_model.dart';
+
 class TrashPage extends StatefulWidget {
-  //const TrashPage({Key? key}) : super(key: key);
   const TrashPage({super.key});
+
   @override
   State<TrashPage> createState() => _TrashPageState();
 }
@@ -50,27 +48,41 @@ class _TrashPageState extends State<TrashPage> {
             }
 
             return ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               itemCount: deletedNotes.length,
               itemBuilder: (context, index) {
                 final note = deletedNotes[index];
+
                 return Card(
-                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  color: const Color(0xffE2DCF2),
+                  elevation: 1,
+                  margin: const EdgeInsets.only(bottom: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
                   child: ListTile(
-                    title: Text(note.title),
-                    subtitle: Text(note.content),
+                    title: Text(
+                      note.title,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      note.content,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         // زر الاسترجاع
                         IconButton(
-                          icon: const Icon(Icons.restore, color: Colors.green),
+                          icon: const Icon(Icons.restore, color:  Color(0xff5C4B7D)),
                           onPressed: () {
                             BlocProvider.of<NotesCubit>(context).Restoring(note);
                           },
                         ),
                         // زر الحذف النهائي
                         IconButton(
-                          icon: const Icon(Icons.delete_forever, color: Colors.red),
+                          icon: const Icon(Icons.delete_forever, color:  Color(0xff5C4B7D)),
                           onPressed: () {
                             BlocProvider.of<NotesCubit>(context).Deletpermenant(note);
                           },
@@ -91,4 +103,3 @@ class _TrashPageState extends State<TrashPage> {
     );
   }
 }
-
